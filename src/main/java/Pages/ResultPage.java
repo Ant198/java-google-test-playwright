6e96e3fd9d1c1d3065ed4b8c6771fdf422c8f3b0
Anton <ridnovir@yahoo.com>
2025-03-13 16:35:14 +0200

    delete unnecessary System.out

--- src/main/java/Pages/ResultPage.java
+++ src/main/java/Pages/ResultPage.java
@@ -7,7 +7,6 @@ import java.util.List;
 public class ResultPage extends PageTools {
     private final String header = "//div[@class='yuRUbf']//a//h3";
     public List<String> getHeaders() {
-        System.out.println(receivePage());
         List<String> headers = receivePage().locator(header).allInnerTexts();
         return headers;
     }
